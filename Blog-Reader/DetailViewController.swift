dfb8a1ac011e9cf17a8de237255a82d7e9327892
Thinh M. Le <thinh.m.le@gmail.com>
2016-01-23 15:52:13 -0800

    Initial Commit

---
+++ Blog-Reader/DetailViewController.swift
@@ -0,0 +1,44 @@
+//
+//  DetailViewController.swift
+//  Blog-Reader
+//
+//  Created by Thinh Le on 1/6/16.
+//  Copyright © 2016 Lac Viet Inc. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var webView: UIWebView!
+
+    var detailItem: AnyObject? {
+        didSet {
+            // Update the view.
+            self.configureView()
+        }
+    }
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = self.detailItem {
+            if let postWebView = self.webView {
+                postWebView.loadHTMLString(detail.valueForKey("content")!.description, baseURL: NSURL(string: "http://googleblog.blogspot.co.uk/"))
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
